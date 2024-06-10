d6227e6d3c0a3a9c021c70c3c2092049294a0d0b
pawellogin <pawellogin712@gmail.com>
2024-06-10 08:58:44 +0200

    Change constructo in Ogloszenia, add auto date and id

--- IOPrototype/src/main/java/org/example/Oglosznenia.java
+++ IOPrototype/src/main/java/org/example/Oglosznenia.java
@@ -1,33 +1,38 @@
 package org.example;
 
+import org.example.Enums.StatusOgloszenia;
+
 import java.time.LocalDateTime;
+
+import static java.time.LocalDateTime.now;
+import static org.example.Enums.StatusOgloszenia.aktywne;
 
 public class Oglosznenia {
 
-
+	private static int idCounter = 0;
 	private String id_ogloszenia;
 	private String tytul;
 	private String opis;
 	private LocalDateTime data_waznosci;
-	private int StatusOgloszenia;
+	private org.example.Enums.StatusOgloszenia StatusOgloszenia;
 	private String typ_ogloszenia;
 	private LocalDateTime data_publikacji;
 
 
-	public Oglosznenia(String id_ogloszenia,
-					   String tytul,
+	public Oglosznenia(String tytul,
 					   String opis,
-					   LocalDateTime data_waznosci,
-					   int statusOgloszenia,
-					   String typ_ogloszenia,
-					   LocalDateTime data_publikacji
+					   String typ_ogloszenia
 	) {
-		this.id_ogloszenia = id_ogloszenia;
+		this.id_ogloszenia = generateId();
 		this.tytul = tytul;
 		this.opis = opis;
-		this.data_waznosci = data_waznosci;
-		StatusOgloszenia = statusOgloszenia;
+		this.data_waznosci = now().plusWeeks(1);
+		this.StatusOgloszenia = aktywne;
 		this.typ_ogloszenia = typ_ogloszenia;
-		this.data_publikacji = data_publikacji;
+		this.data_publikacji = now();
+	}
+
+	private synchronized String generateId() {
+		return String.format("%06d", ++idCounter);
 	}
 }
